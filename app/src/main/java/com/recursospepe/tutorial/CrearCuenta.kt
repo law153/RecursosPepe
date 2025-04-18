e9a21dd369713d6caaea29f8db3e67485a12e42d
Abel <abelx3678@gmail.com>
2025-04-18 16:26:07 -0400

    creado crear cuenta y en Screens

---
+++ app/src/main/java/com/recursospepe/tutorial/CrearCuenta.kt
@@ -0,0 +1,27 @@
+package com.recursospepe.tutorial
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CreearCuentaScreen(navigateToHome: () -> Unit){
+    Column(modifier = Modifier.
+    padding(all = 2.dp).
+    fillMaxSize().
+    background(Color.Blue))
+    {
+        Text("Crear cuenta")
+
+        Button( onClick = { navigateToHome() }) {
+            Text("Navegar a Crear cuenta")
+        }
+    }
+}
--- app/src/main/java/com/recursospepe/tutorial/core/navigation/Screens.kt
+++ app/src/main/java/com/recursospepe/tutorial/core/navigation/Screens.kt
@@ -3,4 +3,5 @@ package com.recursospepe.tutorial.core.navigation
 sealed class Screen(val route: String){
     object Home: Screen("Home")
     object Solicutdes: Screen("Solicitudes")
+    object CrearCuenta: Screen("CrearCuenta")
 }
